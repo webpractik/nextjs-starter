394117fbd7f9d3145874cc638a759c070097b661
Павел Воробьев <pv@webpractik.ru>
2022-11-23 02:17:12 +0300

    feat(next-config): security headers

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 const path = require('path');
 const { withSentryConfig } = require('@sentry/nextjs');
 
-const isSentryEnabled = process.env.NODE_ENV === 'production' && process.env.APP_ENV === 'PROD';
-
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-    enabled: process.env.ANALYZE === 'true',
-});
-
-const cachePublicMaxAge = process.env.CACHE_PUBLIC_MAX_AGE ?? 3600;
-
 /**
  * @type {import('next').NextConfig}
  */
@@ -16,7 +8,7 @@ const nextConfig = {
     sassOptions: {
         indentType: 'tab',
         style: 'compressed',
-        includePaths: [path.join(__dirname, 'src/assets/styles')],
+        includePaths: [path.join(__dirname, 'src/styles')],
         charset: false,
     },
     swcMinify: true,
@@ -41,19 +33,68 @@ const nextConfig = {
             destination: process.env.BACK_INTERNAL_URL ?? '/api/:path*',
         },
     ],
-    headers: async () => [
-        {
-            source: '/:all*(svg|jpg|png|jpeg|woff|woff2|webp|ico)',
-            locale: false,
-            headers: [
-                {
-                    key: 'Cache-Control',
-                    value: `public, max-age=${cachePublicMaxAge}, stale-while-revalidate`,
-                },
-            ],
-        },
-    ],
+    headers: async () => {
+        if (process.env.NODE_ENV !== 'production') return [];
+
+        const CachePublicMaxAge = process.env.CACHE_PUBLIC_MAX_AGE ?? 3600;
+
+        const ContentSecurityPolicy = `
+            default-src 'self';
+            script-src 'self';
+            child-src ${process.env.NEXT_PUBLIC_FRONT_URL};
+            style-src 'self' ${process.env.NEXT_PUBLIC_FRONT_URL};
+            font-src 'self';  
+        `;
+
+        return [
+            {
+                source: '/:all*(svg|jpg|png|jpeg|woff|woff2|webp|ico)',
+                locale: false,
+                headers: [
+                    {
+                        key: 'Cache-Control',
+                        value: `public, max-age=${CachePublicMaxAge}, stale-while-revalidate`,
+                    },
+                ],
+            },
+            {
+                source: '/:path*',
+                headers: [
+                    {
+                        key: 'X-DNS-Prefetch-Control',
+                        value: 'on',
+                    },
+                    {
+                        key: 'Strict-Transport-Security',
+                        value: 'max-age=63072000; includeSubDomains; preload',
+                    },
+                    {
+                        key: 'X-XSS-Protection',
+                        value: '1; mode=block',
+                    },
+                    {
+                        key: 'X-Frame-Options',
+                        value: 'SAMEORIGIN',
+                    },
+                    {
+                        key: 'X-Content-Type-Options',
+                        value: 'nosniff',
+                    },
+                    {
+                        key: 'Content-Security-Policy',
+                        value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim(),
+                    },
+                ],
+            },
+        ];
+    },
 };
+
+const isSentryEnabled = process.env.NODE_ENV === 'production' && process.env.APP_ENV === 'PROD';
+
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: process.env.ANALYZE === 'true',
+});
 
 module.exports = isSentryEnabled
     ? withSentryConfig(nextConfig, { silent: true })
