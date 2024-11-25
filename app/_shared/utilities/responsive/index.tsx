9d846e0d5d2ebc57abd917f404c8ef5bdc735efe
pvorobiev <styrgia@gmail.com>
2024-11-25 22:54:42 +0300

    feat: 🎸 eslint
    bump versions, prevent some rules

--- app/_shared/utilities/responsive/index.tsx
+++ app/_shared/utilities/responsive/index.tsx
@@ -7,28 +7,24 @@ import {
     useTabletMediaQuery,
 } from '~/lib/hooks/use-responsive';
 
-// eslint-disable-next-line sonarjs/function-return-type
 export function Desktop({ children }: { children: ReactNode }): ReactNode {
     const isDesktop = useDesktopMediaQuery();
 
     return isDesktop ? children : null;
 }
 
-// eslint-disable-next-line sonarjs/function-return-type
 export function Laptop({ children }: { children: ReactNode }): ReactNode {
     const isLaptop = useLaptopMediaQuery();
 
     return isLaptop ? children : null;
 }
 
-// eslint-disable-next-line sonarjs/function-return-type
 export function Tablet({ children }: { children: ReactNode }): ReactNode {
     const isTablet = useTabletMediaQuery();
 
     return isTablet ? children : null;
 }
 
-// eslint-disable-next-line sonarjs/function-return-type
 export function Mobile({ children }: { children: ReactNode }): ReactNode {
     const isMobile = useMobileMediaQuery();
 
--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -9,6 +9,9 @@ module.exports = {
     rules: {
         'max-len': 'off',
         'tailwindcss/no-custom-classname': 'off',
+        'sonarjs/function-return-type': 'off',
+        'sonarjs/no-misused-promises': 'off',
+        '@typescript-eslint/no-misused-promises': 'off',
     },
     ignorePatterns: ['.*.js', 'node_modules/'],
     overrides: [{ files: ['*.js?(x)', '*.ts?(x)'], parser: '@typescript-eslint/parser' }],
